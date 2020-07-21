a5968ba6ca5060deab45dfc11fb42942cff17ce0
maximuse <maximuse@windowslive.com>
2020-07-21 23:32:11 +0200

    Initial commit

--- src/hu/maximuseweb/Actions.java
+++ src/hu/maximuseweb/Actions.java
@@ -1,5 +1,33 @@
 package hu.maximuseweb;
 
-public class Actions {
-    
+import java.util.ArrayList;
+
+class Actions {
+    private static ArrayList<DogName> dogNames;
+    private static ArrayList<DogBreed> dogBreeds;
+    private static ArrayList<Dog> dogs;
+
+    private static ArrayList<DogName> getDogNames() {
+        return dogNames;
+    }
+
+    private static void setDogNames(ArrayList<DogName> dogNames) {
+        Actions.dogNames = dogNames;
+    }
+
+    private static ArrayList<DogBreed> getDogBreeds() {
+        return dogBreeds;
+    }
+
+    private static void setDogBreeds(ArrayList<DogBreed> dogBreeds) {
+        Actions.dogBreeds = dogBreeds;
+    }
+
+    private static ArrayList<Dog> getDogs() {
+        return dogs;
+    }
+
+    private static void setDogs(ArrayList<Dog> dogs) {
+        Actions.dogs = dogs;
+    }
 }
